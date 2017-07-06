268e76fd76805aae788726a59d110b4f065f4a94
Fanie Reynders <fanie@reynders.co>
2017-07-06 23:42:14 +0200

    Add files via upload

---
+++ .net.cmd
@@ -0,0 +1,21 @@
+@echo off
+if [%1]==[help] goto help
+REM pushd %~dp0
+"%programfiles%\dotnet\dotnet.exe" %*
+REM popd
+goto end
+
+:help
+echo .NET Core CLI SDK Helper
+echo.
+echo Usage: .net [command]
+echo Usage: .net [dotnet args]
+echo.
+echo Helper commands:
+echo   sdk      Swtiches to a specific .NET Core SDK version
+echo   help     Display help
+echo.
+echo dotnet args:
+echo   The standard dotnet commands for installed sdk
+echo.
+:end
