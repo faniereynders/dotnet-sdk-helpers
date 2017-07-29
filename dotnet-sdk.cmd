c1665258d035e3368ef098fd7135d730e7318038
Sekku <srosasn@gmail.com>
2017-07-29 22:59:04 +0100

    When switching to the latest version, now it looks for a global.json in any of the parent directories and compares the version in it with that of the latest installed on the system. If they differ, prompt the user to choose between them

--- dotnet-sdk.cmd
+++ dotnet-sdk.cmd
@@ -30,20 +30,58 @@ goto end
 
 :sdk_latest
 if exist global.json del global.json
-if exist ..\global.json (
-    set /p choice= There's a global.json in your parent directory. Do you want to delete it? (N/y) 
-    if /I "%choice%"=="y" (
-        del ..\global.json
-    ) else (
-        SETLOCAL ENABLEDELAYEDEXPANSION
-        set dotnetVersion=
-        for /f "delims=" %%a in ('dotnet --version') do set dotnetVersion=%%a
-        echo .NET Core SDK current version: !dotnetVersion!
-        goto end
-    )
-) 
-echo .NET Core SDK version switched to latest version.
+
+REM Look for a global.json file in all the parent directories up to the root
+set currentDir=%cd%
+call :searchParentDirectories
+cd %currentDir%
+
+REM No global.json found in any parent directory, we don't need to do anything more
+if "%version_from_global%"=="" goto end
+
+REM In order to get the latest sdk installed, dotnet.exe --version needs to be executed in a context
+REM where no global.json will be found. I might be assuming too much but I think it's safe to believe
+REM no global.json will be found in %programfiles%\dotnet\
+for /f %%f in ('start /d "%programfiles%\dotnet\" /b dotnet.exe --version') do (
+    set latest_installed_version=%%f
+)
+
+REM The global.json already points to the latest version installed. 
+if "%version_from_global%"=="%latest_installed_version%" goto end
+
+echo You have a global.json in one of your parent directories pointing to the %version_from_global% sdk version
+echo while the latest installed version is the %latest_installed_version% sdk
+echo.
+
+set /p choice= Do you want to switch to the latest installed sdk (%latest_installed_version%)? (Y/n)
+REM Do nothing
+if /I "%choice%"=="n" goto end
+echo Switching .NET Core SDK version to %latest_installed_version% (as a local globa.json)
+(
+echo {
+echo   "sdk": {
+echo     "version": "%latest_installed_version%"
+echo   }
+echo }
+) > global.json
 dotnet --version
+
+goto end
+
+REM Function that goes up in the directory tree looking for a global.json
+REM Stops on the first one found or when the root is reached
+:searchParentDirectories
+cd ..
+if exist global.json ( 
+    for /f %%f in ('dotnet.exe --version') do (
+        set version_from_global=%%f
+    )
+    exit /b
+)
+REM We got to the root, stop recursion
+if "%cd:~3,1%"=="" exit /b
+call :searchParentDirectories
+exit /b
 
 goto end
 
